31fa419a45e446fd3f15433f54f8eb5a3e4f18b7
Dibya Ghosh <djgvolt@gmail.com>
2016-03-19 13:44:35 -0700

    Finished (mostly) scraper

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -4,6 +4,8 @@ from sqlalchemy.orm import sessionmaker
 from models import Post, Comment, Topic
 from piazza_api import Piazza
 from utils import *
+import html
+
 
 class Scraper:
 
@@ -13,6 +15,7 @@ class Scraper:
         self.course = self.piazza.network(Config.courseid)
         self.engine = create_engine('sqlite:///test.db', echo=False)
         self.Session = sessionmaker(bind=self.engine)
+        self.session = self.Session()
 
     def get(self, limit=10):
         for post in self.course.iter_all_posts(limit=limit):
@@ -20,25 +23,47 @@ class Scraper:
             print("Entered")
 
     def process_one(self, post):
-        if len(post['history']) == 0:
-            return
         time = parse_time(post['created'])
-        title = post['history'][0]['subject']
-        body = post['history'][0]['content']
+        title = html.unescape(post['history'][0]['subject'])
+        body = html.unescape(post['history'][0]['content'])
         sqlpost = Post(title, body, time)
         for comment in process_all_children(post):
-            if len(comment['history']) == 0:
-                continue
             time = parse_time(comment['created'])
-            subject = comment['history'][0]['content']
+            if 'history' not in comment:
+                subject = html.unescape(comment['subject'])
+            else:
+                subject = html.unescape(comment['history'][0]['content'])
             sqlpost.children.append(Comment(subject, time))
-        session = self.Session()
-        session.add(sqlpost)
-        session.commit()
+        for topic in post['tags']:
+            sqlpost.topics.append(self.get_topic(topic))
+        self.session.add(sqlpost)
+        self.session.commit()
 
-    def print_all(self):
-        session = self.Session()
-        posts = session.query(Post).all()
+    def get_topic(self, name):
+        """
+        Returns the topic if it exists, and otherwise creates it
+        """
+
+        topic = self.session.query(Topic).filter(Topic.name == name).first()
+        if not topic:
+            topic = Topic(name)
+            self.session.add(topic)
+            self.session.commit()
+        return topic
+
+    def print_posts(self):
+        """
+        Prints a list of all posts currently
+        in the database
+        """
+        posts = self.session.query(Post).all()
         for post in posts:
-            print("Post: ", post)
+            print(post)
+            for comment in post.children:
+                print("\t", comment)
 
+    def print_topics(self):
+        topics = self.session.query(Topic).all()
+        for topic in topics:
+            print(topic)
+           
