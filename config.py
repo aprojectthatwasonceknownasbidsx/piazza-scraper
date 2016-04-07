f100ca5c98c00851219e4b7b465f95ca3daf3be5
Dibya Ghosh <djgvolt@gmail.com>
2016-04-07 10:23:08 -0700

    Removed some unnecsesary details

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ import os
 get = lambda *args: os.environ.get(*args)
 
 class Config:
-    username = get('DB_USERNAME', "dibya.ghosh@g.berkeley.edu")
-    password = get('DB_PASSWORD', "piazzascraper")
-    courseid = get('COURSEID', "ijkj784v4cs5c8")
+    username = get('DB_USERNAME', "")
+    password = get('DB_PASSWORD', "")
+    courseid = get('COURSEID', "")
